2bab9bbb0a5cc1a293cc20e2b5078f18c64223af
Fernando Rios <fermx3@gmail.com>
2025-07-12 06:56:05 -0600

    Change height of home

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold mb-4">Bienvenido a Pitch</h1>
       <p className="text-lg mb-8">
         El futuro de la gestion de inventarios y ventas mayoristas.
